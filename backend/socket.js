04bb465f67513322d0c83d4d696dcee200674d7c
Nguyen Van Truong <truong93540@gmail.com>
2025-08-05 22:54:22 +0700

    edit socket cors

--- backend/socket.js
+++ backend/socket.js
@@ -1,12 +1,15 @@
 import http from 'http'
 import express from 'express'
 import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST'],
     },
 })
