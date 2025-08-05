416d7f5f88409d0a73b2ff9dfae83cf80d8fdd7f
truong93540 <116564609+truong93540@users.noreply.github.com>
2025-08-05 13:36:21 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://mern-instagram-liard.vercel.app/',
         credentials: true,
     }),
 )
